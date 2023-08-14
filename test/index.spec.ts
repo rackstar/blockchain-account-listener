9fe9ff8f34cb1e0ff5e4ee402f55f0546d4146c8
John Doe <john@anonymous.com>
2023-08-14 18:43:01 +0100

    test: Implemented index.spec.ts

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,52 +1,67 @@
+/* eslint @typescript-eslint/no-unused-expressions: 0 */
 import { expect } from "chai";
+import * as logLevel from "console-log-level";
 import { after, afterEach, before, describe, it } from "mocha";
 import { SinonFakeTimers, SinonSandbox, SinonSpy, createSandbox } from "sinon";
+import Indexer from "../src/indexer";
 import { testAccountUpdates } from "./test-data.spec";
 
 describe("Acceptance Criteria Tests", () => {
   let sandbox: SinonSandbox;
   let consoleSpy: SinonSpy;
   let clock: SinonFakeTimers;
+  let indexer: Indexer;
 
   before(() => {
     sandbox = createSandbox();
     consoleSpy = sandbox.spy(console, "info");
     clock = sandbox.useFakeTimers();
+    indexer = new Indexer(logLevel());
   });
-  afterEach(() => consoleSpy.resetHistory());
+  afterEach(() => {
+    indexer = new Indexer(logLevel());
+    consoleSpy.resetHistory();
+  });
   after(() => sandbox.restore());
 
   it("should display a short message log message to when each (accountId + version) tuple has been indexed", () => {
     const [accountUpdate] = testAccountUpdates();
     const { id, version, callbackTimeMs } = accountUpdate;
-    // indexer.index(accountUpdate);
+
+    indexer.listener(accountUpdate);
+
     expect(
       consoleSpy.calledWithMatch(
         `${id} v${version} indexed +${callbackTimeMs}ms`,
       ),
     ).to.be.true;
   });
-
   it("should display a callback log when an account's callbackTimeMs has expired", () => {
     const [accountUpdate] = testAccountUpdates();
     const { id, version, callbackTimeMs } = accountUpdate;
-    // indexer.index(accountUpdate)
+
+    indexer.listener(accountUpdate);
+
     clock.tick(callbackTimeMs + 10);
     expect(consoleSpy.calledWithMatch(`${id} v${version} cb executed`)).to.be
       .true;
   });
   it("should cancel the old callback if it has not fired yet and a new version from the same account is ingested", () => {
     const [accountUpdate1, accountUpdate2] = testAccountUpdates();
-    const { id, version, callbackTimeMs } = accountUpdate1;
-    // indexer.index(accountUpdate1)
+    const { id, version } = accountUpdate1;
+
+    indexer.listener(accountUpdate1);
+
     clock.tick(accountUpdate1.callbackTimeMs - 200);
-    // indexer.index(accountUpdate2)
+    indexer.listener(accountUpdate2);
+
     clock.tick(210);
     expect(
       consoleSpy.calledWithMatch(
         `${id} v${version} cb cancelled by v${accountUpdate2.version}`,
       ),
     ).to.be.true;
+
     clock.tick(accountUpdate2.callbackTimeMs - 200);
     expect(
       consoleSpy.calledWithMatch(
@@ -61,13 +76,15 @@ describe("Acceptance Criteria Tests", () => {
       version: accountUpdate.version - 1,
     };
     const { id, version, callbackTimeMs } = accountUpdate;
-    // indexer.index(accountUpdate);
-    // indexer.index(accountUpdateOld);
+
+    indexer.listener(accountUpdate);
+    indexer.listener(accountUpdateOld);
     clock.tick(callbackTimeMs + 10);
+
     expect(consoleSpy.calledWithMatch(`${id} v${version} cb executed`)).to.be
       .true;
     expect(
-      consoleSpy.notCalledWithMatch(
+      consoleSpy.neverCalledWithMatch(
         `${accountUpdateOld.id} v${accountUpdateOld.version} cb executed`,
       ),
     ).to.be.true;
