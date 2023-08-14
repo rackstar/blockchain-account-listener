0f8376f9586e821153ccdd416ef05a0fcf8e8886
John Doe <john@anonymous.com>
2023-08-14 19:42:09 +0100

    test: Add indexer.spec.ts

---
+++ test/indexer.spec.ts
@@ -0,0 +1,53 @@
+import { expect } from "chai";
+import * as logLevel from "console-log-level";
+import { beforeEach, describe, it } from "mocha";
+import * as sinon from "sinon";
+import Indexer from "../src/indexer";
+import { SolanaAccount } from "../src/types";
+import testAccountUpdates from "./test-data.spec";
+
+describe("Indexer", () => {
+  let indexer: Indexer;
+
+  beforeEach(() => {
+    indexer = new Indexer(logLevel());
+  });
+
+  describe("invalid event", () => {
+    let accountUpdate: SolanaAccount;
+    let infoSpy: sinon.SinonStub;
+    let errorSpy: sinon.SinonStub;
+
+    before(() => {
+      [accountUpdate] = testAccountUpdates();
+      infoSpy = sinon.stub(console, "info");
+      errorSpy = sinon.stub(console, "error");
+    });
+    after(() => sinon.restore());
+
+    it("should return true for valid account updates", () => {
+      const { id, version, callbackTimeMs } = accountUpdate;
+      const isValid = indexer.listener(accountUpdate);
+      expect(isValid).to.be.true;
+      expect(
+        infoSpy.calledWithMatch(
+          `${id} v${version} indexed +${callbackTimeMs}ms`,
+        ),
+      ).to.be.true;
+    });
+
+    it("should return false for invalid account update events", () => {
+      const invalidEvent = { ...accountUpdate, version: undefined }; // making version undefined
+      const isValid = indexer.listener(invalidEvent);
+      expect(isValid).to.be.false;
+      expect(errorSpy.calledWithMatch("Unable to process unexpected event")).to
+        .be.true;
+    });
+
+    it("should push invalid account update events to deadLetterQueue", () => {
+      const invalidEvent = { ...accountUpdate, version: undefined }; // making version undefined
+      indexer.listener(invalidEvent);
+      expect(indexer.getFailedEvents()).to.deep.include(invalidEvent);
+    });
+  });
+});
